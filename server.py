56bfbd8ddc494ead4ed619ee931e0bf6baf78762
kaivalshah47 <kaivalshah47@gmail.com>
2020-08-14 04:16:42 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 13 20:26:44 2020
+
+@author: kaival Shah
+"""
+
+from flask import Flask,request,jsonify
+import util
+
+app=Flask(__name__)
+
+@app.route('/classify_image',methods=['GET','POST'])
+def classify_image():
+    image_data=request.form['image_data']
+    response=jsonify(util.classify_image(image_data))
+    response.headers.add('Access-Control-Allow-Origin','*')
+    
+    return response
+if __name__=="__main__":
+    print("Starting Python Flask Server For Sports Celebrity Image Classification")
+    util.load_saved_artifacts()
+    app.run(port=5000)
